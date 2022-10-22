24dbfe4de6647b8a5884acf283ed42ab4f07e78f
jased <108029829+Jased-0001@users.noreply.github.com>
2022-10-22 17:47:29 -0400

    final touches

--- it-starts-with/one-thing/index.js
+++ it-starts-with/one-thing/index.js
@@ -1,5 +1,10 @@
 window.addEventListener("load", function() {
+    var parsedlyrics;
+
     fetch('https://jased.xyz/it-starts-with/one-thing/lyrics.txt')
-    .then(response=> response.text())
-    .then(text=> document.getElementById('lyrics').innerHTML = text);
+    .then(response => response.text())
+    .then(text => document.getElementById("lyrics").innerHTML = text.replace(/(?:\r\n|\r|\n)/g, "<br>"))
+    .catch(error => console.error(error))
+
+    var lyricz = parsedlyrics
 });
